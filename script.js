9976294b19444a124dfc468ce9e5965c887cfdfb
akshaynarwadkar <akshaynarwadkar1@gmail.com>
2023-01-29 02:45:52 +0530

    removed null exception

--- script.js
+++ script.js
@@ -21,19 +21,42 @@ const balanceEl=document.getElementById('balance')
 
 depositBtnEl.onclick=()=>{
     depositValue=prompt('Enter amount to deposit')
-    akshay.deposit(depositValue)
-    balanceEl.innerHTML= `<p>Current Balance is ${akshay.balance}$</p>`
+    if(depositValue===''){
+        balanceEl.innerHTML= `<p style='color:red' >Enter valid amount to deposit</p>
+    <br>
+    <p><b>Current Balance is ${akshay.balance}$</b></p>`
+    }else if(depositValue===null){
+        balanceEl.innerHTML=`<br><br>
+        <p><b>Current Balance is ${akshay.balance}$</b></p> `
+    }
+    
+    else{
+        akshay.deposit(depositValue)
+        balanceEl.innerHTML= `<p style='color:Blue' >Amount deposited ${depositValue}$</p>
+        <br>
+        <p><b>Current Balance is ${akshay.balance}$</b></p>`
+    }
+   
 }
 
 withdrawBtnEl.onclick=()=>{
     withdrawAmt=prompt('Enter the amount to withdraw')
-    if(akshay.balance>=withdrawAmt){
+    if(withdrawAmt===''){
+        balanceEl.innerHTML=`<p style='color:red' >Enter valid amount to withdraw </p>
+        <br><p><b>Current Balance is ${akshay.balance}$</b><p>`
+    }else if(withdrawAmt===null) {
+        balanceEl.innerHTML=`<br><br>
+        <p><b>Current Balance is ${akshay.balance}$</b></p> `
+    }
+    
+    else if(akshay.balance>=withdrawAmt){
         akshay.withdraw(withdrawAmt)
-        balanceEl.innerHTML=`<p>Current Balance is ${akshay.balance}$<p>`
-    }else{
+        balanceEl.innerHTML=`<p style='color:red' >Amount withdrawn ${withdrawAmt}$ </p>
+        <br><p><b>Current Balance is ${akshay.balance}$</b><p>`
+    }else if(akshay.balance<=withdrawAmt){
         balanceEl.innerHTML=`<p style='color:red' >You do not have sufficient balance to withdraw funds<p>
         <br>
-        <p>Current Balance is ${akshay.balance}$ `
+        <p><b>Current Balance is ${akshay.balance}$</b></p> `
         
     }
 }
